1832a843827bee9699d0b5859f683f91fe059513
Natanael-ADS <natanaelxhd7@gmail.com>
2021-04-27 22:02:18 -0300

    Aula 01 - App Nubank

--- 08 - Flutter/nubank_app/lib/pages/home/home_page.dart
+++ 08 - Flutter/nubank_app/lib/pages/home/home_page.dart
@@ -1,14 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:nubank_app/pages/home/widgets/my_app_bar.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool _ShowMenu;
+
+  @override
+  void initState() {
+    super.initState();
+    _ShowMenu = false;
+  }
+
   @override
   Widget build(BuildContext context) {
+    double _screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: Colors.purple[800],
       body: Stack(
         children: <Widget>[
-          MyAppBar(),
+          MyAppBar(
+            showMenu: _ShowMenu,
+            onTap: () {
+              setState(() {
+                _ShowMenu = !_ShowMenu;
+              });
+            },
+          ),
+          Positioned(
+            top: _screenHeight * 0.17,
+            height: _screenHeight * 0.55,
+            left: 0,
+            right: 0,
+            child: SizedBox(
+              height: _screenHeight * 0.03,
+              child: PageView(
+                children: [
+                  Container(
+                    color: Colors.green,
+                  ),
+                  Container(
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    color: Colors.yellow,
+                  ),
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
--- 08 - Flutter/nubank_app/lib/pages/home/widgets/my_app_bar.dart
+++ 08 - Flutter/nubank_app/lib/pages/home/widgets/my_app_bar.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
 class MyAppBar extends StatelessWidget {
+  final bool showMenu;
+  final VoidCallback onTap;
+
+  const MyAppBar({Key key, this.showMenu, this.onTap}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -8,9 +13,39 @@ class MyAppBar extends StatelessWidget {
         SizedBox(
           height: MediaQuery.of(context).padding.top,
         ),
-        Container(
-          color: Colors.red,
-          height: 200,
+        GestureDetector(
+          onTap: onTap,
+          child: Container(
+              color: Colors.red[800],
+              height: MediaQuery.of(context).size.height * 0.14,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.network(
+                        'https://logodownload.org/wp-content/uploads/2019/08/nubank-logo-3.png',
+                        height: 35,
+                        color: Colors.white,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text('Natanael',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                            color: Colors.white,
+                          )),
+                    ],
+                  ),
+                  Icon(
+                    !showMenu ? Icons.expand_more : Icons.expand_less,
+                    color: Colors.white,
+                  ),
+                ],
+              )),
         )
       ],
     );
