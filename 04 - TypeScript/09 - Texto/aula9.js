24704d597ac2c3c38c9f8b95f46f27ff60bff9d8
Natanael Candido <natanael@sistemaquantum.com.br>
2021-07-13 14:06:30 -0300

    Aula de typescript - assunto - string

---
+++ 04 - TypeScript/09 - Texto/aula9.js
@@ -0,0 +1,7 @@
+var mensagem = 'Hello Word';
+console.log(mensagem.toUpperCase());
+console.log(mensagem.toLowerCase());
+console.log(mensagem.charAt(2));
+var posicao = mensagem.indexOf('Hello');
+var hello = mensagem.slice(0, 5);
+console.log(posicao, hello);
---
+++ 04 - TypeScript/09 - Texto/aula9.ts
@@ -0,0 +1,9 @@
+var mensagem:string = 'Hello Word';
+console.log(mensagem.toUpperCase());
+console.log(mensagem.toLowerCase());
+console.log(mensagem.charAt(2));
+
+var posicao:number = mensagem.indexOf('Hello');
+var hello :string = mensagem.slice(0,5); 
+
+console.log(posicao,hello);
